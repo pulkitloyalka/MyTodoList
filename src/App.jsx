c0ec187c511be311ab6d258be18597bb9cde442a
Pulkit Loyalka <loyalkapulkit@gmail.com>
2025-04-30 14:24:24 +0530

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
     < >
     <Navbar/> 
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
-        <h1 className='font-bold text-center text-3xl'>iTask - Manage your todos at one place</h1>
+        <h1 className='font-bold text-center text-3xl'>My Todo-List - Manage your todos at one place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-2xl font-bold'>Add a Todo</h2>
           <div className="flex">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-around bg-indigo-900 text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-xl mx-8'>iTask</span>
+            <span className='font-bold text-xl mx-8'>My Todo-List</span>
         </div>
       <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
